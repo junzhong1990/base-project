8385831a00f2c4562c655da7ac1fdc0d97dad77a
马涛涛 <547151266@qq.com>
2020-07-23 10:31:12 +0800

    '上传'

---
+++ public/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-input v-model="test"></el-input>
+    <el-button type="primary" @click="testClick">点击</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+        test:''
+      }
+    },
+    methods: {
+      testClick(){
+        this.test = '123'
+      }
+    }
+  }
+</script>
